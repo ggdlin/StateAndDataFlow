d7e640c03eb77ab6348ab88a9f0853e98f0d47f6
Sergey Ivanov <ggdlin@gmail.com>
2020-12-17 09:51:03 +0300

    Initial Commit

---
+++ StateAndDataFlow/StateAndDataFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateAndDataFlowApp.swift
+//  StateAndDataFlow
+//
+//  Created by Ivanov Sergey on 17.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct StateAndDataFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
